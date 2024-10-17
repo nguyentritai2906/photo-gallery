8f98644c757ae3c0745afb35392fc3a093ee4e38
Nguyen Tri Tai <nguyentritai2906@gmail.com>
2024-10-17 09:45:05 +0700

    chore: add my photos

--- examples/src/ExampleWithLightbox.js
+++ examples/src/ExampleWithLightbox.js
@@ -1,7 +1,6 @@
-import React, { useState, useCallback } from 'react';
-import ReactDOM from 'react-dom';
-import Gallery from 'react-photo-gallery';
-import Carousel, { Modal, ModalGateway } from 'react-images';
+import React, { useState, useCallback } from "react";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
 function ExampleWithLightbox({ photos }) {
   const [currentImage, setCurrentImage] = useState(0);
@@ -19,14 +18,13 @@ function ExampleWithLightbox({ photos }) {
 
   return (
     <div>
-      <h2>Using with a Lightbox component</h2>
       <Gallery photos={photos} onClick={openLightbox} />
       <ModalGateway>
         {viewerIsOpen ? (
           <Modal onClose={closeLightbox}>
             <Carousel
               currentIndex={currentImage}
-              views={photos.map(x => ({
+              views={photos.map((x) => ({
                 ...x,
                 srcset: x.srcSet,
                 caption: x.title,
--- examples/src/app.js
+++ examples/src/app.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import jsonp from 'jsonp';
-import ExampleBasic from './ExampleBasic';
-import ExampleWithLightbox from './ExampleWithLightbox';
-import ExampleCustomComponentSelection from './ExampleCustomComponentSelection';
-import ExampleSortable from './ExampleSortable';
-import ExampleDynamicLoading from './ExampleDynamicLoading';
-import ExampleDynamicColumns from './ExampleDynamicColumns';
+import React from "react";
+import ReactDOM from "react-dom";
+import jsonp from "jsonp";
+import ExampleBasic from "./ExampleBasic";
+import ExampleWithLightbox from "./ExampleWithLightbox";
+import ExampleCustomComponentSelection from "./ExampleCustomComponentSelection";
+import ExampleSortable from "./ExampleSortable";
+import ExampleDynamicLoading from "./ExampleDynamicLoading";
+import ExampleDynamicColumns from "./ExampleDynamicColumns";
 
 class App extends React.Component {
   constructor() {
@@ -17,24 +17,27 @@ class App extends React.Component {
   componentDidMount() {
     this.loadPhotos();
   }
+
   loadPhotos() {
     const urlParams = {
-      api_key: '455b5e2fa6b951f9b9ab58a86d5e1f8a',
-      photoset_id: '72157708141247864',
-      user_id: '146659101@N08',
-      format: 'json',
-      per_page: '120',
-      extras: 'url_m,url_c,url_l,url_h,url_o',
+      api_key: "455b5e2fa6b951f9b9ab58a86d5e1f8a",
+      photoset_id: "72177720321288233",
+      user_id: "201696834@N08",
+      format: "json",
+      per_page: "120",
+      extras: "url_m,url_c,url_l,url_h,url_o",
     };
 
-    let url = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos';
+    let url =
+      "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos";
     url = Object.keys(urlParams).reduce((acc, item) => {
-      return acc + '&' + item + '=' + urlParams[item];
+      return acc + "&" + item + "=" + urlParams[item];
     }, url);
 
-    jsonp(url, { name: 'jsonFlickrApi' }, (err, data) => {
-      let photos = data.photoset.photo.map(item => {
+    jsonp(url, { name: "jsonFlickrApi" }, (err, data) => {
+      let photos = data.photoset.photo.map((item) => {
         let aspectRatio = parseFloat(item.width_o / item.height_o);
+        console.log(item);
         return {
           src: item.url_l,
           width: parseInt(item.width_o),
@@ -48,7 +51,7 @@ class App extends React.Component {
             `${item.url_l} ${item.width_l}w`,
             `${item.url_h} ${item.width_h}w`,
           ],
-          sizes: '(min-width: 480px) 50vw, (min-width: 1024px) 33.3vw, 100vw',
+          sizes: "(min-width: 480px) 50vw, (min-width: 1024px) 33.3vw, 100vw",
         };
       });
       this.setState({
@@ -62,24 +65,13 @@ class App extends React.Component {
       const width = this.state.width;
       return (
         <div className="App">
-          <ExampleBasic title={'Basic Row Layout'} photos={this.state.photos.slice(0, 20)} />
-          <ExampleBasic title={'Basic Column Layout'} direction="column" photos={this.state.photos.slice(40, 60)} />
-          <ExampleWithLightbox photos={this.state.photos.slice(60, 75)} />
-          <ExampleCustomComponentSelection photos={this.state.photos.slice(75, 90)} />
-          <ExampleSortable photos={this.state.photos.slice(90, 100)} />
-          <ExampleDynamicColumns title={'Custom Dynamic Columns'} photos={this.state.photos.slice(100, 120)} />
-          <ExampleDynamicLoading photos={this.state.photos} />
+          <ExampleBasic photos={this.state.photos} />
+          <ExampleBasic direction="column" photos={this.state.photos} />
         </div>
       );
     } else {
-      return (
-        <div className="App">
-          <div id="msg-app-loading" className="loading-msg">
-            Loading
-          </div>
-        </div>
-      );
+      return <div className="App"></div>;
     }
   }
 }
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
